a2ef8d9462ca4cdd205c435406de5fc8636dc2af
王朝 <wangzhao90hou@163.com>
2018-02-02 14:49:50 +0800

    change main.css

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,30 +1,33 @@
-var myself =  document.getElementById("myself");
-var iconfont = document.getElementById("iconfont");
-var list = document.getElementById("list");
-var main = document.getElementById("main");
+var myself = document.getElementById("myself"),
+    iconfont = document.getElementById("iconfont"),
+    list = document.getElementById("list"),
+    main = document.getElementById("main"),
+    body = document.getElementsByClassName("body")[0];
+
 var Fadeflag = true;
 
-    function Fade() {
-        this.show = function(obj) {
-	    var num = 0;
-	    if (Fadeflag) {
-	        var st = setInterval(function(){
-	        num++;
-	        Fadeflag = false;
-	        obj.style.opacity = num/10;
-	        if (num>=10) {
-	            clearInterval(st);
-	            Fadeflag = true;
-	            }
-	        },40);
-	    }
-	}
+function Fade() {
+    this.show = function(obj) {
+        var num = 0;
+        if (Fadeflag) {
+            var st = setInterval(function() {
+                num++;
+                Fadeflag = false;
+                obj.style.opacity = num / 10;
+                if (num >= 10) {
+                    clearInterval(st);
+                    Fadeflag = true;
+                }
+            }, 40);
+        }
+    }
 }
-  var fade = new Fade();
-  fade.show(myself);
-  fade.show(iconfont);
-  fade.show(list);
-  fade.show(main);
+var fade = new Fade();
+fade.show(myself);
+fade.show(iconfont);
+fade.show(list);
+fade.show(main);
+// fade.show(body);
 
 // function Slidedown(){
 
